87857e70e9b2b8ddbc79b8c1ac2eeed8a833cdf0
giovaniv <giovaniv@gmail.com>
2018-07-17 03:49:26 +0000

    first commit with main.rb and game.rb

---
+++ game.rb
@@ -0,0 +1,15 @@
+# MAIN FUNCTIONS OF THE GAME
+
+# class player that will play the game
+class player
+  attr_accessor :name, :total_life, :current_life
+  def initialize(name)
+    @name = name
+    @total_life = 3
+    @curr_life = @curr_life
+  end
+end
+
+def start
+  puts "Hello World"
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+# Ruby Math Game 1.0
+# It requires the other files/gems that it needs
+# Created by Giovani Vasconcelos - July 16, 2018
+
+require 'pry'
+require './game'
+
+start
